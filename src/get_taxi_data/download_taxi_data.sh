b4bffc4ddf114673b672af60686f204393b0978d
Carlos Petricioli <carpetri@gmail.com>
2017-10-26 19:02:32 -0400

    to run in dumbo

--- src/get_taxi_data/download_taxi_data.sh
+++ src/get_taxi_data/download_taxi_data.sh
@@ -4,4 +4,13 @@
 # mkdir /Volumes/server-hd/taxi_data
 # cd /Volumes/server-hd/taxi_data
 
-cat taxi_links | parallel -j 8  wget --verbose
+# cat taxi_links | parallel -j 8  wget --verbose
+
+for f in `cat taxi_links`
+do 
+	echo 'Getting: ' $f
+	wget --verbose $f 
+done
+
+mkdir ../../data/taxi_data
+mv -r *.csv ../../data/taxi_data
